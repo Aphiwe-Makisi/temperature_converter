b7fa8da458b9f95c56c41ed346fcb8b14f8b7279
aphiwe-citiq <aphiwem@citiqprepaid.co.za>
2025-08-18 14:52:21 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// This is the entry point for of the application
+}
+
+func convertCelToFah() {}
+
+func convertFahToCel() {}
+
+func convertoCelToKel() {}
+
+func convertKevToCel() {}
+
+func convertFahToKel() {}
+
+func convertKelToFah() {}
+
+func displayMenu() {}
